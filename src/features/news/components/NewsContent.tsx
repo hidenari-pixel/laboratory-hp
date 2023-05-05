1ba0a0fe86e1847f9fd9c0bf9c11413949840b07
hidenari-pixel <hide8800kasu@gmail.com>
2023-05-05 21:08:15 +0900

    newsとresearchのアイテムホバー時のアニメーション追加

--- src/features/news/components/NewsContent.tsx
+++ src/features/news/components/NewsContent.tsx
@@ -18,7 +18,7 @@ export const NewsContent = ({ id, date, title, description, links }: NewsContent
       as="article"
       p="5"
       borderWidth="1px"
-      className="my-3 h-full cursor-pointer rounded-lg hover:bg-slate-100"
+      className="my-3 h-full cursor-pointer rounded-lg transition-all duration-500 hover:scale-[102%] hover:bg-slate-100"
     >
       <LinkBox as="article" p="10" borderWidth="1px" className="h-full rounded-md">
         <NewsImage alt={title} imageName={id} />
@@ -28,7 +28,7 @@ export const NewsContent = ({ id, date, title, description, links }: NewsContent
         <Heading className="my-1 text-xl text-gray-700 decoration-1 underline-offset-4">
           {title}
         </Heading>
-        <Text className="text-sm text-gray-500">{description}</Text>
+        <Text className="whitespace-pre-wrap text-sm text-gray-500">{description}</Text>
         {links && links.length > 0 && <Text className="pt-3 text-sm">関連リンク</Text>}
         {links &&
           links.length > 0 &&
--- src/features/researches/components/ResearchCell.tsx
+++ src/features/researches/components/ResearchCell.tsx
@@ -5,7 +5,12 @@ import { ResearchImage } from './ResearchImage';
 
 export const ResearchCell = ({ id, title, description, links }: Research) => {
   return (
-    <LinkBox as="article" p="5" borderWidth="1px" className="my-3 h-full rounded-lg">
+    <LinkBox
+      as="article"
+      p="5"
+      borderWidth="1px"
+      className="my-3 h-full cursor-pointer rounded-lg transition-all duration-500 hover:scale-[102%] hover:bg-slate-100"
+    >
       <LinkBox as="article" p="10" borderWidth="1px" className="h-full rounded-md">
         <ResearchImage alt={title} itemName={id} />
         <Heading className="my-1 text-xl text-gray-700 decoration-1 underline-offset-4">
