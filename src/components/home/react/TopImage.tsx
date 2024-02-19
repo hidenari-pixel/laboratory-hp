6a7bde3bd706b7d13c33d1818cc34b914714f947
hidenari-pixel <hide8800kasu@gmail.com>
2024-02-19 10:24:14 +0900

    fix height

--- src/components/home/react/TopImage.tsx
+++ src/components/home/react/TopImage.tsx
@@ -6,7 +6,7 @@ const imageWrapperStyle = tv({
 });
 
 const imageStyle = tv({
-	base: "max-w-full w-full transition-all duration-[3000ms] scale-100 lg:block md:block hidden h-screen min-h-screen",
+	base: "max-w-full w-full transition-all duration-[3000ms] scale-100 lg:block hidden h-screen min-h-screen",
 	variants: {
 		isRendered: {
 			true: "scale-125 opacity-20",
