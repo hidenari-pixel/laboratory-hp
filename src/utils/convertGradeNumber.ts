9d3d7681188799af072da18adb60b668f07528ef
hidenari-pixel <hide8800kasu@gmail.com>
2024-08-24 23:52:25 +0900

    fix: メンバーページを修正

--- src/utils/convertGradeNumber.ts
+++ src/utils/convertGradeNumber.ts
@@ -1,20 +1,20 @@
-export const convertGradeNumber = (grade: number, old?: boolean) => {
+export const convertGradeNumber = (year: number, old?: boolean) => {
 	if (old) return "卒業生";
 
-	if (grade === 0) return "ペット";
+	if (year === 0) return "ペット";
 
 	const currentMonth = new Date().getMonth() + 1;
 	const currentYear =
 		currentMonth < 4 ? new Date().getFullYear() - 1 : new Date().getFullYear();
-	const diff = currentYear - grade;
+	const diff = currentYear - year;
 	switch (diff) {
-		case 6:
-			return "修士2年";
 		case 5:
-			return "修士1年";
+			return "修士2年";
 		case 4:
-			return "学士4年";
+			return "修士1年";
 		case 3:
+			return "学士4年";
+		case 2:
 			return "学士3年";
 		default:
 			return "博士";
